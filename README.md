3aed1c3e67c924d81c49b79928bc8e33aa6a1b1d
jetkiwi <jetkiwi@gmail.com>
2013-04-20 01:46:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a test program to check if LWJGL can handle IME input properly.
+
+*Please set VM option to "-Djava.library.path=target/natives"*
---
+++ src/main/java/net/ja731j/lwjgl_test/App.java
@@ -0,0 +1,38 @@
+package net.ja731j.lwjgl_test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+/**
+ * Hello
+ * world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        try {
+            Display.setDisplayMode(new DisplayMode(320, 240));
+            Display.create();
+
+            while (!Display.isCloseRequested()) {
+                while (Keyboard.next()) {
+                    char c = Keyboard.getEventCharacter();
+                    if (c != 0) {
+                        System.out.println(c);
+                    }
+                }
+                Display.update();
+            }
+        } catch (LWJGLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+        } finally {
+            Display.destroy();
+        }
+    }
+}
